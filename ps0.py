0083d7e31a5760709e36798741b3cee99b47e577
d0vuork90 <d0vuork90@gmail.com>
2017-12-02 22:10:30 -0500

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 21:39:45 2017
+
+@author: d0vuork90
+"""
+import math
+x = int(input('Enter number x:'))
+y = int(input('Enter number y:'))
+print('x**y =', x**y)
+print('log(x) =', math.log(x, 2))
